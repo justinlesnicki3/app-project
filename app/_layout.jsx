584920b71a9dee58c7c3f60acf8ead77bf201421
justinlesnicki3 <justinlesnicki5563@aol.com>
2025-01-17 00:06:24 -0600

    Did, a, lot.

---
+++ app/_layout.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import {Slot, Stack} from 'expo-router';
+import "../global.css";
+
+const RootLayout = () => {
+  return (
+   <Stack>
+     <Stack.Screen name="index" options={{ headerShown: false}} />
+   </Stack>
+  ) 
+}
+
+export default RootLayout
---
+++ app/index.jsx
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-7xl">Aoraaa</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile">
+        <Text style={{ color: 'blue' }}>Go back to profile</Text>
+      </Link>
+    </View>
+  );
+}
