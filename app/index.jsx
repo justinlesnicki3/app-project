d0002e002e88898a5c9ca1053f9501a48a6fbaad
justinlesnicki3 <justinlesnicki5563@aol.com>
2025-01-20 22:38:09 -0600

    Worked On Sign up button.

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { Image, ScrollView, Text, View } from 'react-native';
-import { Link } from 'expo-router';
+import { Redirect, router } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 import { images } from '../constants/constants'
+import CustomButton from '../components/CustomButton';
 
 
 export default function App() {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{height: '100%'}}>
-        <View className="w-full justify-center items-center h-full px-4">
+        <View className="w-full justify-center items-center min-h-[85vh] px-4">
           <Image 
             source={images.logo}
             className="w-[130px] h-[84px]"
@@ -35,12 +36,20 @@ export default function App() {
             
             
             />
-
           </View>
 
+          <Text className="text-sm font-preregular text-gray-100 mt-7 text-center">Where creativity meets innovation: embark on a journey of limitless exploration with Aora</Text>
+
+          <CustomButton 
+            title="Continue with Email"
+            handlePress={() => router.push('/sign-in')}
+            containerStyles="w-full mt-7"
+          />
 
         </View>
       </ScrollView>
+
+      <StatusBar backgroundColor='#161622' style='light' />
     </SafeAreaView>
   );
 }
