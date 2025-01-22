75dc0c8bddecad058b7aaeb8b9be235bb23dbe67
justinlesnicki3 <justinlesnicki5563@aol.com>
2025-01-22 13:04:11 -0600

    Worked on signup page.

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,5 +1,6 @@
 import { View, Text } from 'react-native'
 import { Stack } from 'expo-router';
+import{ StatusBar } from 'expo-status-bar'
 import React from 'react'
 
 const AuthLayout = () => {
@@ -9,9 +10,20 @@ const AuthLayout = () => {
         <Stack.Screen 
           name="sign-in"
           options={{
-            headerShown
-          }}/>
+            headerShown: false
+          }}
+          />
+          <Stack.Screen 
+          name="sign-up"
+          options={{
+            headerShown: false
+          }}
+          />
       </Stack>
+
+      <StatusBar backgroundColor="#161622"
+      style="light" />
+
     </>
   )
 }
--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native'
+import React, {useState} from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { images } from '../../constants/constants';
+import FormField from '../../components/FormField';
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: '',
+    password: ''
+  })
+
+
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center min-h-[85vh] px-4 my-6">
+            <Image source={images.logo} resizeMode='contain' className="w-[115px] h-[35px]" />
+
+            <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">Log in to Aora</Text>
+
+            <FormField
+              title="Email"
+              value={form.email}
+              handelChangeText={(e) => setForm({...form,
+                email: e })}
+                otherStyles="mt-7"
+                keyboardTpe="email-address"
+            />
+
+            <FormField
+              title="Password"
+              value={form.password}
+              handelChangeText={(e) => setForm({...form,
+                password: e })}
+                otherStyles="mt-7"
+            />
+
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
