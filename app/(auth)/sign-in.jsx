eb0ef3671bb15c08b7fb014a01aeee25608882ae
justinlesnicki3 <justinlesnicki5563@aol.com>
2025-01-22 15:25:18 -0600

    Worked more on sign up page.

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -3,6 +3,7 @@ import React, {useState} from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../constants/constants';
 import FormField from '../../components/FormField';
+import CustomButton from '../../components/CustomButton'
 
 const SignIn = () => {
   const [form, setForm] = useState({
@@ -10,11 +11,17 @@ const SignIn = () => {
     password: ''
   })
 
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const submit = () => {
+
+  }
+
 
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
-        <View className="w-full justify-center min-h-[85vh] px-4 my-6">
+        <View className="w-full justify-center min-h-[35vh] px-4 my-6">
             <Image source={images.logo} resizeMode='contain' className="w-[115px] h-[35px]" />
 
             <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">Log in to Aora</Text>
@@ -22,19 +29,28 @@ const SignIn = () => {
             <FormField
               title="Email"
               value={form.email}
-              handelChangeText={(e) => setForm({...form,
+              handleChangeText={(e) => setForm({...form,
                 email: e })}
                 otherStyles="mt-7"
-                keyboardTpe="email-address"
+                keyboardType="email-address"
             />
 
             <FormField
               title="Password"
               value={form.password}
-              handelChangeText={(e) => setForm({...form,
+              handleChangeText={(e) => setForm({...form,
                 password: e })}
                 otherStyles="mt-7"
             />
+            <View style={{alignItems: 'center', marginTop: 28}}>
+              <CustomButton 
+              title="Sign In"
+              handlePress={submit}
+              containerStyles="mt-7"
+              isLoading={isSubmitting}
+            />
+            </View>
+            
 
         </View>
       </ScrollView>
