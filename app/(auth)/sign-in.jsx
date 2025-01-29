fd0f7e4834ac39b5c91c5c0985ba586667c05bad
justinlesnicki3 <justinlesnicki5563@aol.com>
2025-01-28 22:29:17 -0600

    Finished Sign up page, also implemented appwrite.

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View, ScrollView, Image } from 'react-native'
 import React, {useState} from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { Link } from 'expo-router'
 import { images } from '../../constants/constants';
 import FormField from '../../components/FormField';
 import CustomButton from '../../components/CustomButton'
@@ -21,7 +22,7 @@ const SignIn = () => {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
-        <View className="w-full justify-center min-h-[35vh] px-4 my-6">
+        <View className="w-full justify-center min-h-[85vh] px-4 my-6">
             <Image source={images.logo} resizeMode='contain' className="w-[115px] h-[35px]" />
 
             <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">Log in to Aora</Text>
@@ -49,6 +50,12 @@ const SignIn = () => {
               containerStyles="mt-7"
               isLoading={isSubmitting}
             />
+            <View className="justify-center pt-5 flex-row gap-2">
+              <Text className="text-lg text-gray-100 font-pregular">
+                Don't have an account?
+              </Text>
+              <Link href="/sign-up" className="text-lg font-psemibold text-secondary">Sign Up</Link>
+            </View>
             </View>
             
 
